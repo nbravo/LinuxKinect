8597227b6e7a5a265d66cfaaaffd1107f46b7cf4
nbravo <nbravo@mit.edu>
2015-03-09 14:58:43 -0400

    micview records from two mics, does not draw both of them yet

--- examples/micview.c
+++ examples/micview.c
@@ -1,29 +1,3 @@
-/*
- * This file is part of the OpenKinect Project. http://www.openkinect.org
- *
- * Copyright (c) 2011 individual OpenKinect contributors. See the CONTRIB file
- * for details.
- *
- * This code is licensed to you under the terms of the Apache License, version
- * 2.0, or, at your option, the terms of the GNU General Public License,
- * version 2.0. See the APACHE20 and GPL2 files for the text of the licenses,
- * or the following URLs:
- * http://www.apache.org/licenses/LICENSE-2.0
- * http://www.gnu.org/licenses/gpl-2.0.txt
- *
- * If you redistribute this file in source form, modified or unmodified, you
- * may:
- *   1) Leave this header intact and distribute it under the same terms,
- *      accompanying it with the APACHE20 and GPL20 files, or
- *   2) Delete the Apache 2.0 clause and accompany it with the GPL2 file, or
- *   3) Delete the GPL v2 clause and accompany it with the APACHE20 file
- * In all cases you must keep the copyright notice intact and include a copy
- * of the CONTRIB file.
- *
- * Binary distributions must follow the binary distribution requirements of
- * either License.
- */
-
 #include "libfreenect.h"
 #include "libfreenect_audio.h"
 #include <stdio.h>
@@ -42,6 +16,7 @@ volatile int die = 0;
 
 static freenect_context* f_ctx;
 static freenect_device* f_dev;
+static freenect_device* f_dev2;
 
 typedef struct {
 	int32_t* buffers[4];
@@ -51,6 +26,7 @@ typedef struct {
 } capture;
 
 capture state;
+capture state2;
 
 int paused = 0;
 
@@ -88,12 +64,43 @@ void in_callback(freenect_device* dev, int num_samples,
 	pthread_mutex_unlock(&audiobuf_mutex);
 }
 
+void in_callback2(freenect_device* dev, int num_samples,
+                 int32_t* mic1, int32_t* mic2,
+                 int32_t* mic3, int32_t* mic4,
+                 int16_t* cancelled, void *unknown) {
+	pthread_mutex_lock(&audiobuf_mutex);
+	capture* c2 = (capture*)freenect_get_user(dev);
+	if(num_samples < c2->max_samples - c2->current_idx) {
+		memcpy(&(c2->buffers[0][c2->current_idx]), mic1, num_samples*sizeof(int32_t));
+		memcpy(&(c2->buffers[1][c2->current_idx]), mic2, num_samples*sizeof(int32_t));
+		memcpy(&(c2->buffers[2][c2->current_idx]), mic3, num_samples*sizeof(int32_t));
+		memcpy(&(c2->buffers[3][c2->current_idx]), mic4, num_samples*sizeof(int32_t));
+	} else {
+		int first = c2->max_samples - c2->current_idx;
+		int left = num_samples - first;
+		memcpy(&(c2->buffers[0][c2->current_idx]), mic1, first*sizeof(int32_t));
+		memcpy(&(c2->buffers[1][c2->current_idx]), mic2, first*sizeof(int32_t));
+		memcpy(&(c2->buffers[2][c2->current_idx]), mic3, first*sizeof(int32_t));
+		memcpy(&(c2->buffers[3][c2->current_idx]), mic4, first*sizeof(int32_t));
+		memcpy(c2->buffers[0], &mic1[first], left*sizeof(int32_t));
+		memcpy(c2->buffers[1], &mic2[first], left*sizeof(int32_t));
+		memcpy(c2->buffers[2], &mic3[first], left*sizeof(int32_t));
+		memcpy(c2->buffers[3], &mic4[first], left*sizeof(int32_t));
+	}
+	c2->current_idx = (c2->current_idx + num_samples) % c2->max_samples;
+	c2->new_data = 1;
+	pthread_cond_signal(&audiobuf_cond);
+	pthread_mutex_unlock(&audiobuf_mutex);
+}
+
 void* freenect_threadfunc(void* arg) {
 	while(!die && freenect_process_events(f_ctx) >= 0) {
 		// If we did anything else in the freenect thread, it might go here.
 	}
 	freenect_stop_audio(f_dev);
 	freenect_close_device(f_dev);
+	freenect_stop_audio(f_dev2);
+	freenect_close_device(f_dev2);
 	freenect_shutdown(f_ctx);
 	return NULL;
 }
@@ -173,7 +180,14 @@ int main(int argc, char** argv) {
 
 	int user_device_number = 0;
 	if (freenect_open_device(f_ctx, &f_dev, user_device_number) < 0) {
-		printf("Could not open device\n");
+		printf("Could not open device 1\n");
+		freenect_shutdown(f_ctx);
+		return 1;
+	}
+
+	int user_device_number2 = 1;
+	if (freenect_open_device(f_ctx, &f_dev2, user_device_number2) < 0) {
+		printf("Could not open device 2\n");
 		freenect_shutdown(f_ctx);
 		return 1;
 	}
@@ -190,8 +204,22 @@ int main(int argc, char** argv) {
 	memset(state.buffers[3], 0, state.max_samples * sizeof(int32_t));
 	freenect_set_user(f_dev, &state);
 
+	state2.max_samples = 256 * 60;
+	state2.current_idx = 0;
+	state2.buffers[0] = (int32_t*)malloc(state2.max_samples * sizeof(int32_t));
+	state2.buffers[1] = (int32_t*)malloc(state2.max_samples * sizeof(int32_t));
+	state2.buffers[2] = (int32_t*)malloc(state2.max_samples * sizeof(int32_t));
+	state2.buffers[3] = (int32_t*)malloc(state2.max_samples * sizeof(int32_t));
+	memset(state2.buffers[0], 0, state2.max_samples * sizeof(int32_t));
+	memset(state2.buffers[1], 0, state2.max_samples * sizeof(int32_t));
+	memset(state2.buffers[2], 0, state2.max_samples * sizeof(int32_t));
+	memset(state2.buffers[3], 0, state2.max_samples * sizeof(int32_t));
+	freenect_set_user(f_dev2, &state2);
+
 	freenect_set_audio_in_callback(f_dev, in_callback);
 	freenect_start_audio(f_dev);
+	freenect_set_audio_in_callback(f_dev2, in_callback2);
+	freenect_start_audio(f_dev2);
 
 	int res = pthread_create(&freenect_thread, NULL, freenect_threadfunc, NULL);
 	if (res) {
